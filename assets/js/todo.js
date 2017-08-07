5c7e77fc48cedb62415efd9ecef936ee9189d658
JeaneC <volbohel@gmail.com>
2017-08-07 12:07:20 -0400

    Commit Markup
    HTML Completed
    - Functionality and JS work, just needs styling.

---
+++ assets/js/todo.js
@@ -0,0 +1,27 @@
+// Check off specific Todos
+
+$("ul").on("click", "li", function(){
+  //if li is gray, turn it to black, else turn it to
+  //grey
+  $(this).toggleClass("completed");
+
+});
+
+//Click on x to delet
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which ===13){
+    var todoText = $(this).val();
+
+    //Create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
+
+});
